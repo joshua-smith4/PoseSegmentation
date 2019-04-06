958e1182655469fe171ea161f4844863d7920259
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-04-06 14:45:34 -0600

    fix appends

--- load_preproc_data.py
+++ load_preproc_data.py
@@ -12,6 +12,7 @@ def load_preproc_data_fcn(fp, train_split=0.8, max_files=100):
     y_test = []
     counter = 0
     for f in files:
+        print(counter)
         if counter >= max_files:
             break
         counter += 1
@@ -22,9 +23,9 @@ def load_preproc_data_fcn(fp, train_split=0.8, max_files=100):
         random_state+=1
         train_divide = int(x.shape[0]*train_split)
         x_train.append(x[:train_divide])
-        x_test = np.append(x[train_divide:])
-        y_train = np.append(y[:train_divide])
-        y_test = np.append(y[train_divide:])
+        x_test.append(x[train_divide:])
+        y_train.append(y[:train_divide])
+        y_test.append(y[train_divide:])
     x_train = np.concatenate(x_train, axis=0)
     y_train = np.concatenate(y_train, axis=0)
     x_test = np.concatenate(x_test, axis=0)
