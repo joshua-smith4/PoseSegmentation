4cb642e28c82e0f61c4251a161897d1e7e6feaf3
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-04-06 16:12:31 -0600

    switch to generators

--- load_preproc_data.py
+++ load_preproc_data.py
@@ -2,19 +2,15 @@ import os
 import numpy as np
 from sklearn.utils import shuffle
 
-def load_preproc_data_fcn(fp, train_split=0.8, max_files=100, training_data=True):
+def load_preproc_generator(fp, train_split=0.8, max_files=100, training_data=True):
     files = [f for f in os.listdir(fp) if f.endswith('.npz')]
     files.sort()
-    x_train = []
-    x_test = []
-    y_train = []
-    y_test = []
     random_state = 0
     counter = 0
     for f in files:
         print(counter)
         if counter >= max_files:
-            break
+            return
         counter += 1
         handle = np.load(os.path.join(fp,f))
         x = handle['x']
@@ -23,25 +19,21 @@ def load_preproc_data_fcn(fp, train_split=0.8, max_files=100, training_data=True
         random_state+=1
         train_divide = int(x.shape[0]*train_split)
         if training_data:
-            x_train.append(x[:train_divide])
-            y_train.append(y[:train_divide])
+            for i in range(train_divide):
+                yield x[i], y[i]
         else:
-            x_test.append(x[train_divide:])
-            y_test.append(y[train_divide:])
-    if training_data:
-        x_train = np.concatenate(x_train, axis=0)
-        y_train = np.concatenate(y_train, axis=0)
-    else:
-        x_test = np.concatenate(x_test, axis=0)
-        y_test = np.concatenate(y_test, axis=0)
-    if training_data:
-        return x_train, y_train
-    return x_test, y_test
-    # return x_train, y_train, x_test, y_test
+            for i in range(train_divide, x.shape[0]):
+                yield x[i], y[i]
 
 
-def load_preproc_data_rf(fp, wx, wy, pad="edge"):
-    pass
+def load_preproc_generator_windowed(fp, wx, wy, pad="edge", padArgs=None, train_split=0.8, max_files=100, training_data=True):
+    g = load_preproc_generator(fp, train_split=train_split, max_files=max_files, training_data=training_data)
+    assert (wx % 2 != 0 and wy % 2 != 0), "width and height of sliding window must be odd integers"
+    for x,y in g:
+        paddedX = np.pad(x, [(wy//2,), (wx//2,)], pad, **padArgs)
+        for i in x.shape[1]:
+            for j in x.shape[0]:
+                yield paddedX[i:i+wy,j:j+wx], y[i,j]
 
 if __name__ == '__main__':
     x_train, y_train = load_preproc_data_fcn("/home/jsmith/ubc3v_preproc", max_files=60)
