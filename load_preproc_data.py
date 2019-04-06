67172a51df89dd19a03c54f366aa258efeeafbb2
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-04-06 14:56:20 -0600

    separate train and test load because of size

--- load_preproc_data.py
+++ load_preproc_data.py
@@ -2,14 +2,14 @@ import os
 import numpy as np
 from sklearn.utils import shuffle
 
-def load_preproc_data_fcn(fp, train_split=0.8, max_files=100):
+def load_preproc_data_fcn(fp, train_split=0.8, max_files=100, training_data=True):
     files = [f for f in os.listdir(fp) if f.endswith('.npz')]
     files.sort()
-    random_state = 0
     x_train = []
     x_test = []
     y_train = []
     y_test = []
+    random_state = 0
     counter = 0
     for f in files:
         print(counter)
@@ -22,15 +22,22 @@ def load_preproc_data_fcn(fp, train_split=0.8, max_files=100):
         x, y = shuffle(x, y, random_state=random_state)
         random_state+=1
         train_divide = int(x.shape[0]*train_split)
-        x_train.append(x[:train_divide])
-        x_test.append(x[train_divide:])
-        y_train.append(y[:train_divide])
-        y_test.append(y[train_divide:])
-    x_train = np.concatenate(x_train, axis=0)
-    y_train = np.concatenate(y_train, axis=0)
-    x_test = np.concatenate(x_test, axis=0)
-    y_test = np.concatenate(y_test, axis=0)
-    return x_train, y_train, x_test, y_test
+        if training_data:
+            x_train.append(x[:train_divide])
+            y_train.append(y[:train_divide])
+        else:
+            x_test.append(x[train_divide:])
+            y_test.append(y[train_divide:])
+    if training_data:
+        x_train = np.concatenate(x_train, axis=0)
+        y_train = np.concatenate(y_train, axis=0)
+    else:
+        x_test = np.concatenate(x_test, axis=0)
+        y_test = np.concatenate(y_test, axis=0)
+    if training_data:
+        return x_train, y_train
+    return x_test, y_test
+    # return x_train, y_train, x_test, y_test
 
 
 def load_preproc_data_rf(fp, wx, wy, pad="edge"):
