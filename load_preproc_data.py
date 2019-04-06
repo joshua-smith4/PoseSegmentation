fe187876430c9c1457791a141839026939dc349a
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-04-06 16:26:39 -0600

    remove main

--- load_preproc_data.py
+++ load_preproc_data.py
@@ -36,5 +36,5 @@ def load_preproc_generator_windowed(fp, wx, wy, pad="edge", padArgs=None, train_
                 yield paddedX[i:i+wy,j:j+wx], y[i,j]
 
 if __name__ == '__main__':
-    for x,y in load_preproc_generator("/home/jsmith/ubc3v_preproc", training_data=True):
-        print(x.shape, y.shape)
+    # for x,y in load_preproc_generator("/home/jsmith/ubc3v_preproc", training_data=True):
+    #     print(x.shape, y.shape)
