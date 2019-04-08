bf0b70969d01cdef0f4964197715464b88833b4a
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-04-07 22:24:23 -0600

    wip1

--- sliding_window_rf.py
+++ sliding_window_rf.py
@@ -12,13 +12,14 @@ from tensorflow.keras.utils import to_categorical
 
 
 def make_rf_dataset(config):
-    g = load_preproc_generator_windowed(config['path_to_ubc3v'], config['window_size_x'], config['window_size_y'])
+    g = load_preproc_generator_windowed(
+        config['path_to_ubc3v'], config['window_size_x'], config['window_size_y'])
+
     def cast_to_tf_gen():
-        for x,y in g:
-            a = tf.convert_to_tensor(x,dtype=tf.float32,name="input_x")
-            print(a.shape)
-            yield tf.convert_to_tensor(x,dtype=tf.float32,name="input_x"), tf.convert_to_tensor(y,dtype=tf.int32,name="input_y")
-    return Dataset.from_generator(cast_to_tf_gen, (tf.float32, tf.int32))
+        for x, y in g:
+            yield tf.convert_to_tensor(x, dtype=tf.float32, name="input_x"), tf.convert_to_tensor(y, dtype=tf.int32, name="input_y")
+    return Dataset.from_generator(cast_to_tf_gen, (tf.float32, tf.int32),
+                                  (tf.TensorShape([config['window_size_x'] * config['window_size_y']]), tf.TensorShape([])))
 
 
 def build_estimator(config):
