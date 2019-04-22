403689658129e2ebe4ded6bc3e61e5aaef9cfa5a
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-04-22 14:55:25 -0600

    print shapes

--- cnn_model.py
+++ cnn_model.py
@@ -4,6 +4,28 @@ from keras.layers import Conv2D, Input, MaxPooling2D, Dropout, Conv2DTranspose, 
 from keras.models import Model
 from load_preproc_data import load_preproc_generator
 
+from keras.objectives import *
+from keras.metrics import binary_crossentropy
+import keras.backend as K
+import tensorflow as tf
+
+
+# Softmax cross-entropy loss function for pascal voc segmentation
+# and models which do not perform softmax.
+# tensorlow only
+def softmax_sparse_crossentropy_ignoring_last_label(y_true, y_pred):
+    y_pred = K.reshape(y_pred, (-1, K.int_shape(y_pred)[-1]))
+    log_softmax = tf.nn.log_softmax(y_pred)
+
+    y_true = K.one_hot(tf.to_int32(K.flatten(y_true)), K.int_shape(y_pred)[-1]+1)
+    unpacked = tf.unstack(y_true, axis=-1)
+    y_true = tf.stack(unpacked[:-1], axis=-1)
+
+    cross_entropy = -K.sum(y_true * log_softmax, axis=1)
+    cross_entropy_mean = K.mean(cross_entropy)
+
+    return cross_entropy_mean
+
 
 def cnn_model_fn_keras():
     input_shape = (424,512)
@@ -77,7 +99,7 @@ if __name__ == '__main__':
 
     model = cnn_model_fn_keras()
     model.compile(
-        loss=keras.losses.sparse_categorical_crossentropy,
+        loss=softmax_sparse_crossentropy_ignoring_last_label,
         optimizer='sgd',
         metrics=['accuracy'],
     )
