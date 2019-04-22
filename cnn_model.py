d6e24b83a63889d7eafc455ae86d290e237e8355
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-04-22 14:33:01 -0600

    print shapes

--- cnn_model.py
+++ cnn_model.py
@@ -8,35 +8,58 @@ from load_preproc_data import load_preproc_generator
 def cnn_model_fn_keras():
     input_shape = (424,512)
     input_layer = Input(shape=input_shape,dtype='float32',name='input_layer')
+    print(input_layer.shape)
     in_reshape = Reshape(input_shape + (1,))(input_layer)
+    print(in_reshape.shape)
     conv1 = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv2d_1')(in_reshape)
+    print(conv1.shape)
     conv2 = Conv2D(64, (3, 3), activation='relu', padding='same', name='conv2d_2')(conv1)
+    print(conv2.shape)
     pool1 = MaxPooling2D((2,2),strides=(2,2))(conv2)
+    print(pool1.shape)
     # 212 256
     conv3 = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv2d_3')(pool1)
+    print(conv3.shape)
     conv4 = Conv2D(256, (3, 3), activation='relu', padding='same', name='conv2d_4')(conv3)
+    print(conv4.shape)
     pool2 = MaxPooling2D((2,2),strides=(2,2))(conv4)
+    print(pool2.shape)
     # 106 128
     conv5 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv2d_5')(pool2)
+    print(conv5.shape)
     conv6 = Conv2D(512, (3, 3), activation='relu', padding='same', name='conv2d_6')(conv5)
+    print(conv6.shape)
     pool3 = MaxPooling2D((2,2),strides=(2,2))(conv6)
+    print(pool3.shape)
     # 53 64
     up1 = Lambda(lambda x: tf.image.resize_bilinear(x,(106,128)))(pool3)
+    print(up1.shape)
     deconv1 = Conv2DTranspose(512, (3,3), activation='relu', padding='same', name='deconv1')(up1)
+    print(deconv1.shape)
     deconv2 = Conv2DTranspose(512, (3,3), activation='relu', padding='same', name='deconv2')(deconv1)
+    print(deconv2.shape)
 
     up2 = Lambda(lambda x: tf.image.resize_bilinear(x,(212,256)))(deconv2)
+    print(up2.shape)
     deconv3 = Conv2DTranspose(256, (3,3), activation='relu', padding='same', name='deconv3')(up2)
+    print(deconv3.shape)
     deconv4 = Conv2DTranspose(256, (3,3), activation='relu', padding='same', name='deconv4')(deconv3)
+    print(deconv4.shape)
 
     up3 = Lambda(lambda x: tf.image.resize_bilinear(x,(424,512)))(deconv4)
+    print(up3.shape)
     add_input = keras.layers.add([up3,in_reshape])
+    print(add_input.shape)
     deconv5 = Conv2DTranspose(64, (3,3), activation='relu', padding='same', name='deconv5')(add_input)
+    print(deconv5.shape)
     deconv6 = Conv2DTranspose(64, (3,3), activation='relu', padding='same', name='deconv6')(deconv5)
+    print(deconv6.shape)
 
     deconv7 = Conv2DTranspose(1, (3,3), activation='relu', padding='same', name='deconv7')(deconv6)
+    print(deconv7.shape)
 
     model = Model(input_layer,deconv7)
+    print(model.output_shape)
     return model
 
 
