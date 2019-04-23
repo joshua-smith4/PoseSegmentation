31c6ce83a5d90af02ec4e1052804849c1cd20a45
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-04-22 21:42:24 -0600

    sparse categorical with 46 channel output

--- cnn_model.py
+++ cnn_model.py
@@ -44,10 +44,10 @@ def cnn_model_fn_keras():
     print(up1.shape)
     logits = Conv2DTranspose(46, (3,3), activation='softmax', padding='same', name='deconv1')(up1)
     print(logits.shape)
-    # flat_logits = Reshape((424*512*46,1))(logits)
+    flat_logits = Reshape((424*512*46,1))(logits)
     probs = keras.activations.softmax(logits, axis=3)
     print(probs.shape)
-    model = Model(input_layer,logits)
+    model = Model(input_layer,flat_logits)
     print(model.output_shape)
     return model
 
