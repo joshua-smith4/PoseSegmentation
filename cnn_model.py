c05007d55261a1ba52927c073260e328d5227e96
Joshua Smith <joshua.smith4@aggiemail.usu.edu>
2019-04-23 11:57:22 -0600

    start documentation

--- cnn_model.py
+++ cnn_model.py
@@ -4,32 +4,46 @@ from keras.layers import Conv2D, Input, MaxPooling2D, Dropout, Conv2DTranspose, 
 from keras.models import Model
 from load_preproc_data import load_preproc_generator
 
-
+# create keras model of proposed segmentation architecture FCN
 def cnn_model_fn_keras():
+    # the input shape of the ubc3v dataset
     input_shape = (424,512)
     input_layer = Input(shape=input_shape,dtype='float32',name='input_layer')
+    # keras expects data to be in the format (input_shape,) + (channels,)
     in_reshape = Reshape(input_shape + (1,))(input_layer)
+    # convolution layers to extract features
     conv1 = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv2d_1')(in_reshape)
     conv2 = Conv2D(32, (3, 3), activation='relu', padding='same', name='conv2d_2')(conv1)
+    # max pooling for dimension reduction
     pool1 = MaxPooling2D((2,2),strides=(2,2))(conv2)
-    # 212 256
+    # up sampling to return to input dimension size
     up1 = Lambda(lambda x: tf.image.resize_bilinear(x,(424,512)))(pool1)
+    # add input to mitigate loss of resolution through upsampling
     added = keras.layers.add([up1,in_reshape])
+    # output is a tensor 424 x 512 x 46 (46 class probabilities per pixel)
     logits = Conv2DTranspose(46, (3,3), activation='softmax', padding='same', name='deconv1')(added)
+    # flatten output for loss function
     flat_logits = Reshape((424*512,46))(logits)
+    # return keras model
     model = Model(input_layer,flat_logits)
     return model
 
+# function used to convert network output to class labels
 def model_predict(model, x):
     y = model.predict(x)
+    y = y.reshape(-1, 424, 512, 46)
     y = np.argmax(y, axis=3)
     return y
 
+# loss function that ignores background class (0) to weight training
 def categorical_crossentropy_ignore_first_2d(y_true, y_pred):
     return keras.losses.categorical_crossentropy(y_true[:,:,1:],y_pred[:,:,1:])
 
+# accuracy calculation that ignores background class (0)
 def categorical_accuracy_ignore_first_2d(y_true, y_pred):
     return keras.metrics.categorical_accuracy(y_true[:,:,1:],y_pred[:,:,1:])
+
+# call python cnn_model.py to either continue or create and train above model
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
